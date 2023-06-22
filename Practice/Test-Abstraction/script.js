554e4850538c078a6c85f8016391d4204bd5768b
03OCTMWFWD-Divyesh <gajnotardivyesh@gmail.com>
2023-06-22 18:29:44 +0530

    Adv-js

--- Practice/Test-Abstraction/script.js
+++ Practice/Test-Abstraction/script.js
@@ -5,12 +5,15 @@ class User {
     constructor() {
         console.log("This is default constructor");
     }
+
     #validateEmail(email) {
         return true;
     }
+
     #validatePassword(password) {
         return true;
     }
+
     signUp(name, email, password) {
         let isValidate = false;
         isValidate = this.#validateEmail(email);
@@ -24,24 +27,45 @@ class User {
             console.log("Plz entered details again!!");
         }
     }
-    login(email, password){
-        if(email === this.email && password === this.#password){
+
+    login(email, password) {
+        if (email === this.email && password === this.#password) {
             console.log("Login succsessfully");
-        }else{
+        } else {
             console.log("Login Failed");
         }
     }
-    resetPassword(email, newPassword){
-        if(this.email === email){
+
+    resetPassword(email, newPassword) {
+        if (this.email === email) {                                                 // this.email = email
             this.#password = newPassword;
             console.log("Operation successfully");
-        }else{
+        } else {
             console.log("Halti pikino tha");
         }
     }
-}
+
+
+
+
+    // * Biji rit :-
+
+    // resetPassword(email, newPassword){
+    //     if (email === this.email){
+    //         // newPassword = this.#password;
+    //         // newPassword === this.#password;
+    //         // this.#password = newPassword;
+    //         this.#password === newPassword;
+    //         console.log("Reset successfully...");
+    //     }else{
+    //         console.log("Chalaja");
+    //     }
+
+    // }
+};
+
 const obj = new User();
 obj.signUp("king", "king@gmail.com", 123456);
-obj.login ("king@gmail.com", 123456);
+obj.login("king@gmail.com", 123456);
 obj.resetPassword("king@gmail.com", "king18");
-// obj.login("king@gmail.com", "king18");
+obj.login("king@gmail.com", "king18");

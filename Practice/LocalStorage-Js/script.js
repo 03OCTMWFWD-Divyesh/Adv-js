ed500157ac693a6d7a59c281b94a8a0da9ccad4e
03OCTMWFWD-Divyesh <gajnotardivyesh@gmail.com>
2023-07-11 15:19:04 +0530

    Adv js

---
+++ Practice/LocalStorage-Js/script.js
@@ -0,0 +1,28 @@
+// * Form Data :-
+            // LocalStorage using (1) Session & (2) Webstorage.
+ 
+// (1) Form Data to local storage :-
+
+// let form = document.getElementById('form');
+
+// form.addEventListener ('submit', (e) => {
+//     e.preventDefault();
+//     let username = document.getElementById ('username').value;
+//     let password = document.getElementById ('password').value;
+
+
+//     // * LocalStorage :-
+
+//     localStorage.setItem ('username', username);
+//     localStorage.setItem ('password', password);
+
+//     alert ("Data submitted successfully");
+// }); 
+
+
+
+
+
+
+
+

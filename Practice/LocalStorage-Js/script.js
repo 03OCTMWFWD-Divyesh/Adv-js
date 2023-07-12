f26c9b8e3f8b61b6d36f89e2a70001a6426cb995
03OCTMWFWD-Divyesh <gajnotardivyesh@gmail.com>
2023-07-12 15:27:14 +0530

    Adv js

--- Practice/LocalStorage-Js/script.js
+++ Practice/LocalStorage-Js/script.js
@@ -22,6 +22,35 @@
 
 
 
+// (2) Form ma Data batave refresh karye na jai :-
+
+
+// let form = document.getElementById('form');
+
+// if(localStorage.getItem('username') && localStorage.getItem('password')){
+//     document.getElementById('username').value = localStorage.getItem('username');
+//     document.getElementById('password').value = localStorage.getItem('password');
+// };
+
+
+// form.addEventListener ('submit', (e) => {
+//     e.preventDefault();
+//     let username = document.getElementById ('username').value;
+//     let password = document.getElementById ('password').value;
+
+
+//     // * LocalStorage :-
+
+//     localStorage.setItem ('username', username);
+//     localStorage.setItem ('password', password);
+
+//     alert ("Data submitted successfully");
+// }); 
+
+
+
+
+
 
 
 
